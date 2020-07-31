ef359b0a2fc9b64c38152f69291fca3a575f2e89
Thisal De Silva <thisal@yaalalabs.com>
2020-07-31 11:03:53 +0530

    shell

---
+++ myscript.sh
@@ -0,0 +1,28 @@
+export MY_COOL_NODE_APP=$HOME/Documents/generateApp/
+
+echo enter files path like /Documents/openapiyamls
+read search_dir
+# =$HOME/Documents/openapiyamls/
+
+echo enter output filename
+read myfile
+
+
+
+
+for entry in "$HOME/$search_dir"/*
+do
+file $entry
+curl -F "myapi=@$entry;type=text/yaml" https://aqueous-retreat-41952.herokuapp.com/uploadopenapi
+done
+
+curl https://aqueous-retreat-41952.herokuapp.com/downloadDocument -o $myfile.zip
+
+
+
+
+
+
+
+
+# node $MY_COOL_NODE_APP/index.js 
